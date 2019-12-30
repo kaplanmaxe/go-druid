98d86364f6d5371af9891f9bbdbf6192a6469ce4
Max Kaplan <max@maxkaplan.me>
2019-12-29 22:40:01 -0500

    statement, transaction: return proper unimplemented error

--- statement.go
+++ statement.go
@@ -7,22 +7,27 @@ import (
 
 type statementNoop struct{}
 
+// Close is a noop as druid is not an OLTP DB
 func (s *statementNoop) Close() (err error) {
-	return
+	return driver.ErrSkip
 }
 
+// NumInput is a noop as druid is not an OLTP DB
 func (s *statementNoop) NumInput() (num int) {
 	return
 }
 
+// ExecContext is a noop as druid is not an OLTP DB
 func (s *statementNoop) ExecContext(ctx context.Context, args []driver.Value) (driver.Result, error) {
-	return &result{}, nil
+	return &result{}, driver.ErrSkip
 }
 
+// Exec is a noop as druid is not an OLTP DB
 func (s *statementNoop) Exec(args []driver.Value) (driver.Result, error) {
-	return &result{}, nil
+	return &result{}, driver.ErrSkip
 }
 
+// Query is a noop as druid is not an OLTP DB
 func (s *statementNoop) Query(args []driver.Value) (driver.Rows, error) {
-	return &rows{}, nil
+	return &rows{}, driver.ErrSkip
 }
--- transaction.go
+++ transaction.go
@@ -1,11 +1,15 @@
 package druid
 
+import "database/sql/driver"
+
 type transactionNoop struct{}
 
+// Commit is a noop that as druid is not an OLTP DB
 func (t *transactionNoop) Commit() (err error) {
-	return
+	return driver.ErrSkip
 }
 
+// Rollback is a noob as druid is not an OLTP DB
 func (t *transactionNoop) Rollback() (err error) {
-	return
+	return driver.ErrSkip
 }
